45a3bc973b2aaa96ffdfa3687dc799279f86b697
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-01 19:39:45 +0200

    Add recursive call which subdivides porblem

--- fibonacci/fib.rb
+++ fibonacci/fib.rb
@@ -3,5 +3,6 @@ class Fib
   def self.num_at_pos(pos)
     return 0 if pos.zero?
     return 1 if pos == 1
+    self.num_at_pos(pos-2) + self.num_at_pos(pos-1)
   end
 end
--- spec/fibonacci/fib_spec.rb
+++ spec/fibonacci/fib_spec.rb
@@ -4,11 +4,11 @@ require_relative "../../fibonacci/fib"
 RSpec.describe Fib do
   describe "#num_at_pos" do
     context "when asking for base cases" do
-      xit "returns 0 when asked for number at position 0" do
+      it "returns 0 when asked for number at position 0" do
         expect(Fib.num_at_pos(0)).to eql(0)
       end
 
-      xit "returns 1 when asked for number at position 1" do
+      it "returns 1 when asked for number at position 1" do
         expect(Fib.num_at_pos(1)).to eql(1)
       end
     end
@@ -16,7 +16,7 @@ RSpec.describe Fib do
     context "when asking for a random number" do
       fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,	987, 1597, 2584, 4181]
 
-      xit "returns number from the fibonacci sequence at the specified position" do
+      it "returns number from the fibonacci sequence at the specified position" do
         random_pos = rand(2..fibonacci.size)
         expect(Fib.num_at_pos(random_pos)).to eql(fibonacci[random_pos])
       end
