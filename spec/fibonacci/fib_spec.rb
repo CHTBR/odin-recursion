9bbe8b414b0f7e6df2fe687615749f93aa3750c3
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-01 19:18:50 +0200

    Add tests for base cases for recursive function

--- spec/fibonacci/fib_spec.rb
+++ spec/fibonacci/fib_spec.rb
@@ -3,5 +3,12 @@ require_relative "../../fibonacci/fib"
 
 RSpec.describe Fib do
   describe "#num_at_pos" do
+    context "when asking for base cases" do
+      xit "returns 0 when asked for number at position 0" do
+      end
+
+      xit "returns 1 when asked for number at position 1" do
+      end
+    end
   end
 end
