50594ef9bfbb453729f9554cbbdff6f6f6a18f2c
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 13:42:52 +0200

    Rename recursive fib implementation and add new tests for it

--- fibonacci/fib_recursive.rb
+++ fibonacci/fib_recursive.rb
@@ -1,8 +1,9 @@
 # Class for storing fibonacci sequence related methods
-class Fib
+class FibRecursive
   def self.num_at_pos(pos)
     return 0 if pos.zero?
     return 1 if pos == 1
-    self.num_at_pos(pos-2) + self.num_at_pos(pos-1)
+
+    num_at_pos(pos - 2) + num_at_pos(pos - 1)
   end
 end
