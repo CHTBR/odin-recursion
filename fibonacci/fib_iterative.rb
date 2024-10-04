98abf27af123bccf77b100bd6e82654e2d4a9d8b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 13:51:55 +0200

    Add base condition for pos 0

--- fibonacci/fib_iterative.rb
+++ fibonacci/fib_iterative.rb
@@ -1,3 +1,6 @@
 # A class for iteratively calculating numbers from fibonacci sequence and returning them as an array
 class FibIterative
+  def self.num_at_pos(pos)
+    return 0 if pos.zero?
+  end
 end
