9b6c8ff002b53b3b3d49f34119696aa648056109
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 14:00:57 +0200

    Move new recursive implementation to correct file

--- fibonacci/fib_recursive.rb
+++ fibonacci/fib_recursive.rb
@@ -1,9 +1,11 @@
 # Class for storing fibonacci sequence related methods
 class FibRecursive
   def self.num_at_pos(pos)
-    return 0 if pos.zero?
-    return 1 if pos == 1
+    return [0] if pos.zero?
+    return [0, 1] if pos == 1
 
-    num_at_pos(pos - 2) + num_at_pos(pos - 1)
+    arr1 = num_at_pos(pos - 1)
+    arr2 = num_at_pos(pos - 2)
+    arr1 << (arr1.last + arr2.last)
   end
 end
