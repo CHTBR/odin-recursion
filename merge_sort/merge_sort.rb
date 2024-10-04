4218641cd4b089fd510cb1ceee5be3506268c105
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 15:52:52 +0200

    Add method for merging two subarrays

--- merge_sort/merge_sort.rb
+++ merge_sort/merge_sort.rb
@@ -5,5 +5,27 @@ module MergeSort
 
     arr1 = sort(arr[0..(arr.size / 2 - 1)])
     arr2 = sort(arr[(arr.size / 2)..-1])
+    _merge(arr1, arr2)
+  end
+
+  def self._merge(arr1, arr2)
+    k = 0
+    l = 0
+    result_arr = []
+    while k < arr1.size && l < arr2.size
+      if arr1[k] < arr2[l]
+        result_arr << arr1[k]
+        k += 1
+      else
+        result_arr << arr2[l]
+        l += 1
+      end
+    end
+    if arr1[k].nil?
+      result_arr += arr2[l..]
+    elsif arr2[l].nil?
+      result_arr += arr1[k..]
+    end
+    result_arr
   end
 end
