7158df0616a597ac1000fc21ae6fc4f823e2b651
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 13:40:20 +0200

    Include general tests for fib

--- spec/fibonacci/fib_iterative_spec.rb
+++ spec/fibonacci/fib_iterative_spec.rb
@@ -3,4 +3,7 @@ require_relative "fib_shared"
 require_relative "../../fibonacci/fib_iterative"
 
 RSpec.describe FibIterative do
+  it_behaves_like "a fibonacci generator" do
+    let(:generator) { subject }
+  end
 end
