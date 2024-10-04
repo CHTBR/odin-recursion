3e76dc809fcfe1e7bf39fe9e49d48fb90ee4b1b8
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 13:57:59 +0200

    Add recursive calls and script to pass an array of results

--- fibonacci/fib_iterative.rb
+++ fibonacci/fib_iterative.rb
@@ -1,7 +1,11 @@
 # A class for iteratively calculating numbers from fibonacci sequence and returning them as an array
 class FibIterative
   def self.num_at_pos(pos)
-    return 0 if pos.zero?
-    return 1 if pos == 1
+    return [0] if pos.zero?
+    return [0, 1] if pos == 1
+
+    arr1 = num_at_pos(pos - 1)
+    arr2 = num_at_pos(pos - 2)
+    arr1 << (arr1.last + arr2.last)
   end
 end
