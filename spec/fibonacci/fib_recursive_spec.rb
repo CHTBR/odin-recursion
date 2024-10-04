d4582fc09ccf1785adf4037a9c3231601169b04d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 13:46:40 +0200

    Include general fib tests

--- spec/fibonacci/fib_recursive_spec.rb
+++ spec/fibonacci/fib_recursive_spec.rb
@@ -3,4 +3,7 @@ require_relative "fib_shared"
 require_relative "../../fibonacci/fib_recursive"
 
 RSpec.describe FibRecursive do
+  it_behaves_like "a fibonacci generator" do
+    let(:generator) { FibRecursive }
+  end
 end
